578d0659723fb37d99368f91432afdf764e70ec9
unknown <owinobenbellah@gmail.com>
2024-07-04 16:51:25 +0300

    ADD:-> 'Create team ' form

---
+++ app/src/routes/esports/team/create/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+
+</script>
+
+
+<main class="flex flex_center items-center w-full h-full">
+<form class="main_border flex flex_col items-center w-96 mt-6 " action="" method="POST">
+        <div class="formField flex flex_row">
+            <label for="name">Name: </label>
+            <input type="text" name="name" id="name">
+        </div>
+
+        <div class="formField flex flex_row justify-start">
+            <label for="game">Game: </label>
+            <input type="text" name="game" id="game">
+        </div>
+
+        <div class="formField flex flex_row">
+            <label for="manager">Manager: </label>
+            <input type="text" name="manager" id="manager">
+        </div>
+
+        <div class="formField flex flex_row"> 
+            <label for="region"> Region: </label>
+            <input type="text" name="region" id="region">
+        </div>
+
+        <button type="submit" class="main_border mt-5 p-1 rounded-lg main_bg_hover secondary_txt_hover">create team</button>
+    </form>
+</main>
+
+<style>
+
+</style>
